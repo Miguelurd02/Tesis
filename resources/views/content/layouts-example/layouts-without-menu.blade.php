6f4f40179ca73c6e095282568b61d9f05f5c545b
Miguel <miguelurdanetar02@gmail.com>
2023-05-17 13:56:30 -0400

    Inicio de sesion con home

--- resources/views/content/layouts-example/layouts-without-menu.blade.php
+++ resources/views/content/layouts-example/layouts-without-menu.blade.php
@@ -30,7 +30,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <!-- <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Inscríbete hoy</a></li> -->
-                        <li class="nav-item"><a class="nav-link" href="iniciarsesionusuario.html">Acceso usuarios</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/auth/login-basic">Acceso usuarios</a></li>
                         <li class="nav-item"><a class="nav-link" href="iniciarsesioninmo.html">Acceso inmobiliarias</a></li>
                     </ul>
                 </div>
